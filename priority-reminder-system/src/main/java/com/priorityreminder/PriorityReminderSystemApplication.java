55dde2c6a0132ed831e5bc6a3907246b3b7803b1
Abhishek Gupta <ag739018@gmail.com>
2025-12-26 17:22:45 +0530

    Initial Setup of the Repository

---
+++ priority-reminder-system/src/main/java/com/priorityreminder/PriorityReminderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.priorityreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityReminderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityReminderSystemApplication.class, args);
+	}
+
+}
---
+++ priority-reminder-system/src/test/java/com/priorityreminder/PriorityReminderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priorityreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorityReminderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
